16096d86dc472cafc553d250375e772e366fc6ff
prihodulkin <45002548+prihodulkin@users.noreply.github.com>
2020-05-14 20:06:24 +0300

    Рома+

--- app/src/main/java/com/example/arrivalmessage/InfoActivity.java
+++ app/src/main/java/com/example/arrivalmessage/InfoActivity.java
@@ -12,6 +12,7 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        addListenerOnButton();
     }
 
     public void addListenerOnButton() {
